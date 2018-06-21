af918d952bb3ad6a9eb9d4b657ce233cb1f3e64d
srajgopal <srajgopal@hotmail.com>
2018-06-21 17:19:36 -0400

    commit

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions  options) : base(options)
         {
             
